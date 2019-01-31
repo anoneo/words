9df7fc26890311c28531ced4a15c656f50f4541c
Anon Praditkhai <anon.pra@kbtg.tech>
2019-01-31 10:12:57 +0700

    first t1

---
+++ words.go
@@ -0,0 +1,15 @@
+// words/words.go
+package words
+
+import (
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s) // HL
+	r := map[string]int{}
+	for _, w := range words {
+		r[w] = r[w] + 1 // HL
+	}
+	return r
+}
